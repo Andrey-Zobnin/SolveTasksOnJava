ee9c231ecf12cdc5fc1af95b96fa76d5816e768a
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-04-24 11:08:06 +0300

    add  all test done solution

---
+++ src/sort/MedianOfArray.java
@@ -0,0 +1,19 @@
+package sort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MedianOfArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] array = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(array);
+        System.out.println(array[N / 2]);
+    }
+}
